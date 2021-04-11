06a86a2a62c1b90ee1cb02b2be30707c9dfe8188
eugeniamanak <69486823+eugeniamanak@users.noreply.github.com>
2021-04-11 13:46:03 -0300

    Update ex5.py

--- ex5.py
+++ ex5.py
@@ -1,16 +1,16 @@
-myName = "Eugenia"
-myAge = 23
-myHeight = round(74*2.54) #inches to centimetres 
-myWeight = round(180/2.20) # lbs to kg
-myEyes = "green"
-myTeeth = "white"
-myHair = "brown"
+name = "Eugenia"
+age = 23
+height = round(74*2.54) #inches to centimetres 
+weight = round(180/2.20) # lbs to kg
+eyes = "green"
+teeth = "white"
+hair = "brown"
 
-print (f"Let's talk about {myName}. ")
-print (f"I'm {myHeight} centimeters tall. ")
-print (f" I'm {myWeight} kg heavy. ")
+print (f"Let's talk about {name}. ")
+print (f"I'm {height} centimeters tall. ")
+print (f" I'm {weight} kg heavy. ")
 print (f"Actually that´s not too heavy. ")
-print (f"I'm got {myEyes} eyes and {myHair} hair. ")
-print (f"My teeths are usually {myTeeth} depending on the coffe")
-total = myAge + myHeight + myWeight 
-print (f"If I add {myAge}, {myHeight}, and {myWeight} I get {total}")
+print (f"I'm got {eyes} eyes and {hair} hair. ")
+print (f"My teeths are usually {teeth} depending on the coffe")
+total = age + height + weight 
+print (f"If I add {age}, {height}, and {weight} I get {total}")
