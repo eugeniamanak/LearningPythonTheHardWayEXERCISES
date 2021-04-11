7526b386099757614e7eb16e442560f0158516b5
eugeniamanak <69486823+eugeniamanak@users.noreply.github.com>
2021-04-11 17:06:51 -0300

    Create ex12Inputs.py

---
+++ ex12Inputs.py
@@ -0,0 +1,4 @@
+age = input("How old are you? ") # this line defines the variable age, ask and collects the info needed
+height = input("How tall are you? ")
+weight = input("How much so you wheigh? ") 
+print(f"So, you're {age} old, {height} tall and {weight} heavy")
