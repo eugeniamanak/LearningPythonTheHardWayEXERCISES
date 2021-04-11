b8e4439b05251f8332aede5aa93eb8f6409f3598
eugeniamanak <69486823+eugeniamanak@users.noreply.github.com>
2021-04-11 13:37:44 -0300

    create ex5.py

---
+++ ex5.py
@@ -0,0 +1,16 @@
+myName = "Eugenia"
+myAge = 23
+myHeight = round(74*2.54) #inches to centimetres 
+myWeight = round(180/2.20) # lbs to kg
+myEyes = "green"
+myTeeth = "white"
+myHair = "brown"
+
+print (f"Let's talk about {myName}. ")
+print (f"I'm {myHeight} centimeters tall. ")
+print (f" I'm {myWeight} kg heavy. ")
+print (f"Actually that´s not too heavy. ")
+print (f"I'm got {myEyes} eyes and {myHair} hair. ")
+print (f"My teeths are usually {myTeeth} depending on the coffe")
+total = myAge + myHeight + myWeight 
+print (f"If I add {myAge}, {myHeight}, and {myWeight} I get {total}")
